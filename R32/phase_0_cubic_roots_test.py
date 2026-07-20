336edc76f46fbca036dda4ecb19a7e597f6a9f5d
snarasi2 <snarasi2@nd.edu>
2026-07-20 12:36:41 -0400

    Phase 0 checkpoint

--- R32/phase_0_cubic_roots_test.py
+++ R32/phase_0_cubic_roots_test.py
@@ -1,6 +1,6 @@
 """
 This code serves as a test to see that IDAES is able to solve the cubic equations
-of state with 0 degrees of freedom.
+of state for all methods within the Colon group collaboration with 0 degrees of freedom.
 
 Date created: 07/20/2026
 
@@ -47,7 +47,10 @@ from idaes.models.properties.modular_properties.phase_equil.forms import log_fug
 R = 8.314 # Real gas constant, J/mol/K
 MW = 52.024e-03 # kg/mol Molar mass of CH₂F₂, from standard atomic weights
 #(NIST WebBook: 52.024 g/mol).
-
+# Antoine: log10(P_bar) = A - B/(T_K + C)  -- saturation-pressure init guess, fit to Linde
+Antoine_A = 4.60123
+Antoine_B = 959.89766   # K
+Antoine_C = -13.71589   # K
 ## Defining all the properties 
 # "omega" = Pitzer acentric factor, computed per method from the Linde vapor
 # pressure at Tr = 0.7:  omega = -1 - log10(Psat(Tr=0.7) / Pc), using each
@@ -79,9 +82,52 @@ def make_config(p):
                 "cp_mol_ig_comp": NIST, # Shomate ideal-gas Cp
                 "enth_mol_ig_comp": NIST, # Shomate ideal-gas enthalpy
                 "entr_mol_ig_comp": NIST, # Shomate ideal-gas entropy
-                "pressure_sat_comp": NIST  # Antoine saturation pressure (init guess)
+                "pressure_sat_comp": NIST,  # Antoine saturation pressure (init guess)
+                "phase_equilibrium_form": {("Vap","Liq"): log_fugacity}, ## VLE form
+                "parameter_data": {
+                    "mw": (MW, pyunits.kg/pyunits.mol),
+                    "pressure_crit": (p["Pc"], pyunits.Pa),
+                    "temperature_crit": (p["Tc"], pyunits.K),
+                    "omega": p["omega"],
+                    "cp_mol_ig_comp_coeff":{
+                        "A": (p["A"], pyunits.J/pyunits.mol/pyunits.K),
+                        "B": (p["B"], pyunits.J/pyunits.mol/pyunits.K/pyunits.kiloK),
+                        "C": (p["C"], pyunits.J/pyunits.mol/pyunits.K/pyunits.kiloK**2),
+                        "D": (p["D"], pyunits.J/pyunits.mol/pyunits.K/pyunits.kiloK**3),
+                        "E": (p["E"], pyunits.J*pyunits.kiloK**2/pyunits.mol/pyunits.K),
+                        "F": (0.0, pyunits.kJ/pyunits.mol),
+                        "G": (0.0, pyunits.J/pyunits.mol/pyunits.K),
+                        "H": (0.0, pyunits.kJ/pyunits.mol),
+                    },
+                    "pressure_sat_comp_coeff":{
+                        "A": (Antoine_A, None),
+                        "B": (Antoine_B, pyunits.K),
+                        "C": (Antoine_C, pyunits.K),
+                    },
+                },
             }
-        }
+        },
+        "phases": {
+            "Liq": {"type": LiquidPhase, "equation_of_state": Cubic,
+                    "equation_of_state_options": {"type": CubicType.PR}},
+            "Vap": {"type": VaporPhase, "equation_of_state": Cubic,
+                    "equation_of_state_options": {"type": CubicType.PR}},
+        },
+        "base_units": {"time": pyunits.s, "length": pyunits.m, "mass": pyunits.kg,
+                       "amount": pyunits.mol, "temperature": pyunits.K},
+        "state_definition": FTPx,
+        "state_bounds": {
+            "flow_mol": (0.0, 1.0, 1000.0, pyunits.mol/pyunits.s),
+            "temperature": (200.0, 300.0, 450.0, pyunits.K),
+            "pressure": (1.0e4, 1.0e5, 1.0e7, pyunits.Pa),
+        },
+        "pressure_ref": (1.0e5, pyunits.Pa),
+        "temperature_ref": (298.15, pyunits.K),
+        "include_enthalpy_of_formation": False,
+        "phases_in_equilibrium": [("Vap", "Liq")],
+        "phase_equilibrium_state": {("Vap", "Liq"): SmoothVLE},
+        "bubble_dew_method": LogBubbleDew,
+        "parameter_data": {"PR_kappa": {("R32", "R32"): 0.000}},
     }
 
 
