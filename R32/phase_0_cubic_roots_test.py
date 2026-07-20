c0458fc51bc88052a5147f8dca2241bac5094455
snarasi2 <snarasi2@nd.edu>
2026-07-20 11:19:30 -0400

    Phase 0: IDAES generic PR property package cubic-roots solve test

---
+++ R32/phase_0_cubic_roots_test.py
@@ -0,0 +1,11 @@
+"""
+This code serves as a test to see that IDAES is able to solve the cubic equations
+of state with 0 degress of freedom.
+
+Date created: 07/20/2026
+
+Author: Shilpa Narasimhan
+
+Support: Claude AI
+
+"""
